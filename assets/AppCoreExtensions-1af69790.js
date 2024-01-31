c10ebe327ac39109939d11149164c4609615e998
andiez-dungpv <pvdung@humaxdigital.com>
2024-01-31 10:53:39 +0700

    implement sprint_2

---
+++ assets/AppCoreExtensions-1af69790.js
@@ -0,0 +1,2 @@
+import{C as a,W as e,S as s}from"./CoreExtension-3fd98810.js";function r(){import.meta.url,import("_").catch(()=>1);async function*o(){}}const t="./";class c extends a{async run(n){n.fontManager.addFontFace(new e("NotoSansJP",{},"/fonts/NotoSansJP-Regular.ttf")),n.fontManager.addFontFace(new s("Ubuntu",{},"msdf",n,t+"fonts/Ubuntu-Bold.msdf.png",t+"fonts/Ubuntu-Bold.msdf.json"))}}export{r as __vite_legacy_guard,c as default};
+//# sourceMappingURL=AppCoreExtensions-1af69790.js.map
