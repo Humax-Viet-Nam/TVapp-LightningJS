ac450ee98296dd9eb0accb18467a80557dfe3cb3
andiez-dungpv <pvdung@humaxdigital.com>
2024-02-20 17:01:33 +0700

    update

---
+++ assets/AppCoreExtensions-6a23bca2.js
@@ -0,0 +1,2 @@
+import{C as o,W as a}from"./CoreExtension-584164f0.js";function e(){import.meta.url,import("_").catch(()=>1);async function*t(){}}class s extends o{async run(n){n.fontManager.addFontFace(new a("NotoSansJP",{},"/fonts/NotoSansJP-Regular.ttf"))}}export{e as __vite_legacy_guard,s as default};
+//# sourceMappingURL=AppCoreExtensions-6a23bca2.js.map
