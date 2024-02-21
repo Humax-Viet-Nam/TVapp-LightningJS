ab313ea4b593d5f296eeb4e57da573d197e32a66
andiez-dungpv <pvdung@humaxdigital.com>
2024-02-21 17:41:02 +0700

    update

--- assets/AppCoreExtensions-30b7b6f2.js
+++
@@ -1,2 +0,0 @@
-import{C as e,W as n}from"./CoreExtension-584164f0.js";function a(){import.meta.url,import("_").catch(()=>1);async function*o(){}}class s extends e{async run(t){t.fontManager.addFontFace(new n("Roboto",{},"/fonts/Roboto-Regular.ttf"))}}export{a as __vite_legacy_guard,s as default};
-//# sourceMappingURL=AppCoreExtensions-30b7b6f2.js.map
